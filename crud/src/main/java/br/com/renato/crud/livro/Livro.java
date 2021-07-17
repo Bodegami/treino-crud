3feb6b3ad30f5d850128590e326082a28542fdd6
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-07-17 09:34:41 -0300

    add Entity Livro

---
+++ crud/src/main/java/br/com/renato/crud/livro/Livro.java
@@ -0,0 +1,76 @@
+package br.com.renato.crud.livro;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "livros")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String autor;
+
+    private String isbn;
+
+    private BigDecimal preco;
+
+    @Deprecated
+    public Livro() { }
+
+    public Livro(String nome, String autor, String isbn, BigDecimal preco) {
+        this.nome = nome;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", autor='" + autor + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(getId(), livro.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
